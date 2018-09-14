aa2de503135c158eb69cf5a1ca1fd65b7d40d111
Mark Benschop <mark.benschop@rws.nl>
2018-09-14 14:23:33 +0200

    updated debian_initial_setup.sh. Now installs docker 18.03.1

--- debian_initial_setup.sh
+++ debian_initial_setup.sh
@@ -59,8 +59,16 @@ EOF
 
 
 echo 'Enabling ipv4 forwarding' && \
-cat <<EOF > /etc/systemctl.d/ipv4_forward.conf
+cat <<EOF > /etc/sysctl.d/ipv4_forward.conf
 net.ipv4.forward.all=1
 EOF
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
+echo 'Removing docker.io' && \
+apt-get remove docker docker-engine docker.io
+
+echo 'Installing docker dependencies' && apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+echo 'Addeing docker gpgp key' && curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+echo 'Adding docker repository' && add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $( lsb_release -cs) stable"
+echo 'Updating apt cache' && apt-get update
+echo 'Installing docker-ce' && apt-get install -y docker-ce=18.03.1~ce-0~debian
