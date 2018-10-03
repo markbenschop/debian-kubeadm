6baef9591b77890566f13155986152a6cca8119e
Mark Benschop <markbenschop@users.noreply.github.com>
2018-10-03 11:07:19 +0200

    updated 03-prometheus.sh

--- 03-prometheus.sh
+++ 03-prometheus.sh
@@ -3,7 +3,8 @@
 DIR='prometheus-operator/contrib/kube-prometheus/manifests/'
 git clone https://github.com/coreos/prometheus-operator.git 
 
-if [ -d "${DIR}" ]
-         kubectl create -f ${DIR} || true
-         kubectl create -f ${DIR}/ 2>/dev/null  || true
+if [ -d ${DIR} ]
+    then
+       kubectl create -f ${DIR} || true
+       kubectl create -f ${DIR}/ 2>/dev/null  || true
 fi
