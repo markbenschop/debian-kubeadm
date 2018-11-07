cf175f34d387495d458edcbbba8db1a2ba6414bd
mark <mark@benschop.it>
2018-11-07 18:43:03 +0100

    added ingress rule for traefik webui to 04-traefik.sh

--- 04-traefik.sh
+++ 04-traefik.sh
@@ -1,5 +1,7 @@
 
 #!/bin/bash
 
+echo 'Deploy traefik' 
 kubectl apply -f https://raw.githubusercontent.com/containous/traefik/master/examples/k8s/traefik-deployment.yaml
-
+echo 'Create an ingress rule for traefik webgui'
+kubectl apply -f https://raw.githubusercontent.com/containous/traefik/master/examples/k8s/ui.yaml
